2fccb3e178ea601aed4eaca35473dd013ddd6200
hansenwt2 <wayne.todd.hansen@gmail.com>
2015-10-30 20:33:59 -0400

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gwJpmc8App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gwJpmc8App
+ */
+angular.module('gwJpmc8App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gwJpmc8App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gwJpmc8App
+ */
+angular.module('gwJpmc8App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
