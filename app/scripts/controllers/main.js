0e84aba57f4945a0ce3fff5606fce84a971d57a6
Sean Strauss <seandstrauss@gmail.com>
2015-10-30 23:25:32 -0400

    added some directives

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,42 @@
  * Controller of the gwJpmc8App
  */
 angular.module('gwJpmc8App')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
+
+  	function Dress() {
+  		
+  	}
+
+  	function Bottoms(){
+
+  	}
+  	function Top() {
+
+  	}
+
+  	function Shoes() {
+
+  	}
+  	
+  	$scope.laundreyList = [];
+
+
+  	$scope.addDress() {
+  		$scope.laundreyList.push(new Dress());
+  	}
+
+  	$scope.addBottoms() {
+  		$scope.laundreyList.push(new Bottoms);
+  	}
+
+  	$scope.addTop() {
+  		$scope.laundreyList.push(new Top);
+  	}
+
+  	$scope.addShoes() {
+  		$scope.laundreyList.push(new Shoes);
+  	}
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
