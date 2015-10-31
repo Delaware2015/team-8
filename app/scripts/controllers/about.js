5043f9dfbd330e3c27603b24d4d4438163415062
hansenwt2 <wayne.todd.hansen@gmail.com>
2015-10-31 01:57:17 -0400

    Added deductions calculator front end and consumer tracker front end

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,12 @@
  * Controller of the gwJpmc8App
  */
 angular.module('gwJpmc8App')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AboutCtrl', function ($scope, $user) {
+    $scope.submitForm = function(isValid) {
+
+    // check to make sure the form is completely valid
+    if (isValid) {
+      alert('our form is amazing');
+    }
+};
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,12 +15,4 @@ angular.module('gwJpmc8App')
       'Karma'
     ];
 
-    for(var i=1; i<=24; i++){
-	    var select = document.getElementById("hours");
-	    var option = document.createElement("OPTION");
-		select.options.add(option);
-		option.text = i;
-		option.value = i;
-	}
-	
   });
